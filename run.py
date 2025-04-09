a943f8a6c6e17801db375be4c308e2887bc0d41b
Toby Pasquale <pasqut@amazon.com>
2025-04-08 19:52:19 -0700

    Ensure db.create_all() runs in production context

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
 
+# This makes sure we create tables with the correct DB config
+if __name__ != '__main__':
+    with app.app_context():
+        db.create_all()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
