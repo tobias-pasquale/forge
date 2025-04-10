357c55910375e10f9ef0c6e72f140a3a923ae615
Toby Pasquale <pasqut@amazon.com>
2025-04-09 21:51:09 -0700

    Update Flask-Login dependency and refactor user loading in extensions

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,11 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 from flask_login import LoginManager
-from backend.models.user import User
-
-login_manager = LoginManager()
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+login_manager = LoginManager()
+
